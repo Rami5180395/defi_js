1fdee77b81447a77ff3778cf1f6f6aa01714edde
Rami5180395 <79589768+Rami5180395@users.noreply.github.com>
2021-03-31 09:11:23 +0100

    Add files via upload

---
+++ brief.js
@@ -0,0 +1,45 @@
+//ex1
+function permut(a,b){
+    var tmp
+        tmp = a
+        a = b
+        b = tmp
+return "la valeur de a est: " + a + " et la valeur de b est: " + b
+}
+console.log(permut(5,9));
+
+//ex2
+   var nom=Toto,
+   var age="30",
+   var homme=true
+console.log("jhkh"+nom+"jkhkjh"+age+"hjhgjhg"+homme)
+
+//ex3
+function puissance(a)
+    return a**2;
+console.log(puissance(2))
+
+//ex1
+function somme(a,b)
+return a+b;
+
+//ex3
+function minutetosecond(a)
+return a*60
+
+ //ex4
+ function increment(a)
+ return a+1
+
+//ex5
+function surface(a,b)
+return (a*b)/2
+
+//ex6
+let s = 'WayToLearnX';
+let rev = s.split('').reverse().join('');
+console.log(rev);
+
+
+
+
